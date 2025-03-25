702f62edebd2335303f713d703f075042f5e77a3
Apoorv Pathak <apoorvworks@gmail.com>
2025-03-25 12:51:56 +0100

    add simplemde for issue description textarea

--- src/app/issues/new/page.tsx
+++ src/app/issues/new/page.tsx
@@ -1,6 +1,8 @@
 "use client"
 import React from 'react'
 import { Button, TextArea, TextField } from '@radix-ui/themes'
+import SimpleMDE from "react-simplemde-editor"
+import "easymde/dist/easymde.min.css"
 
 function NewIssuePage() {
   return (
@@ -8,7 +10,7 @@ function NewIssuePage() {
         <p className='font-semibold text-xl'>Create new issue</p>
         <TextField.Root placeholder="Title">
         </TextField.Root>
-        <TextArea placeholder='description'></TextArea>
+        <SimpleMDE placeholder='description'></SimpleMDE>
         <Button>Submit New Issue</Button>
     </div>
   )
